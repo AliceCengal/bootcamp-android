d2f6172d2cc458e1588f0e10ea8429f7d60d4379
AliceCengal <cengallut@gmail.com>
2014-08-22 12:29:56 -0500

    scala skeleton project

---
+++ app/src/main/scala/com/vandyapps/bootcamp/Main.scala
@@ -0,0 +1,14 @@
+package com.vandyapps.bootcamp
+
+import android.app.Activity
+import android.os.Bundle
+
+class Main extends Activity {
+
+  override def onCreate(saved: Bundle) {
+    super.onCreate(saved)
+    setContentView(R.layout.activity_main)
+  }
+  
+}
+
