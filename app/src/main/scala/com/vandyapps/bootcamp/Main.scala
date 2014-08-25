6fdfbf66d4ccb38433a87a0a1ea6a2350c05f0cf
AliceCengal <cengallut@gmail.com>
2014-08-24 21:45:37 -0500

    Some view demo

--- app/src/main/scala/com/vandyapps/bootcamp/Main.scala
+++ app/src/main/scala/com/vandyapps/bootcamp/Main.scala
@@ -1,13 +1,22 @@
 package com.vandyapps.bootcamp
 
-import android.app.Activity
-import android.os.Bundle
+import android.widget._
+import org.scaloid.common._
 
-class Main extends Activity {
+class Main extends SActivity {
 
-  override def onCreate(saved: Bundle) {
-    super.onCreate(saved)
+  def button = find[Button](R.id.button)
+  def text = find[TextView](R.id.textView)
+
+  onCreate {
     setContentView(R.layout.activity_main)
+    text.setText("Right")
+    button.onClick(switch())
+  }
+
+  def switch() {
+    if (text.getText == "Right") text.setText("Left")
+    else text.setText("Right")
   }
   
 }
